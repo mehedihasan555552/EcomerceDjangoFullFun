44924485d132ceb3d17a055ee683e8b0514aa4fd
Mehedi Hasan <mehedihasan555552@gmail.com>
2021-05-20 02:43:58 +0600

    update

--- MehediEcomerceShop/settings.py
+++ MehediEcomerceShop/settings.py
@@ -160,16 +160,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
 
 STATIC_URL = '/static/'
 
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    BASE_DIR / 'static'
 ]
 
 MEDIA_URL = '/images/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = BASE_DIR / 'static/images'
